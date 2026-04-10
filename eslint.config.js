291a15682efaf1207e834c285e919700a5c7cff6
DESKTOP-BETG49P\A <fishwwww0516@naver.com>
2026-04-10 13:03:12 +0900

    fix: fix eslint error (#13)

--- eslint.config.js
+++ eslint.config.js
@@ -8,21 +8,28 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([globalIgnores(['dist']), {
-  files: ['**/*.{ts,tsx}'],
-  extends: [
-    js.configs.recommended,
-      {
-        rules: {
-          "react-refresh/only-export-components": "off",
-        },
-      },
-    tseslint.configs.recommended,
-    reactHooks.configs.flat.recommended,
-    reactRefresh.configs.vite,
-  ],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+export default defineConfig([
+  globalIgnores(['dist']),
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
   },
-}, ...storybook.configs["flat/recommended"]])
+
+  ...storybook.configs["flat/recommended"],
+  {
+    files: ['src/components/ui/**/*.tsx'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  },
+])
