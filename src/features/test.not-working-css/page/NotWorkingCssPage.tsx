527ad5fb8f9449bc271425a1705b08d3166b5a3d
Tynahan <nusilite@gmail.com>
2026-04-10 19:15:36 +0900

    feat: 비정상작동 css에 width 추가 (#21)

--- src/features/test.not-working-css/page/NotWorkingCssPage.tsx
+++ src/features/test.not-working-css/page/NotWorkingCssPage.tsx
@@ -1,8 +1,12 @@
 const NotWorkingCssPage = () => {
   return (
     <div>
-      <div className="bg-amber-500 p-5">p-5</div>
-      <div className="bg-amber-500 p-[100px]">p-[100px]</div>
+      <div className="bg-amber-500 p-5 w-200">p-5 w-200</div>
+      <div className="bg-amber-500 p-[100px] w-[800px]">
+        p-[100px] w-[800px]
+      </div>
+      <div className="bg-amber-500 p-5 w-xl">p-5 w-xl</div>
+      <div className="bg-amber-500 p-[100px] w-lg">p-[100px] w-lg</div>
     </div>
   )
 }
