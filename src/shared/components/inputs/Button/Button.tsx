7335a86e158425318a947eec1262385866ed2c04
ThePott <134694901+ThePott@users.noreply.github.com>
2026-04-13 15:20:17 +0900

    feat: 공통 컴포넌트 버튼 제작 (#33) (#38)
    * feat: temp - add button props (#33)
    
    * feat: 버튼 초안 작성 (#33)
    
    * design: 피그마에 맞게 tailwindcss 수정 (#33)
    
    * design: isContained에서 hover 배경색 추가 (#33)

--- src/shared/components/inputs/Button/Button.tsx
+++ src/shared/components/inputs/Button/Button.tsx
@@ -1,12 +1,66 @@
 import type { DefaultButtonProps } from "@/shared/types"
+import { cva } from "class-variance-authority"
+import clsx from "clsx"
 import type { ReactNode } from "react"
 
+const buttonVariants = cva("transition", {
+  variants: {
+    size: {
+      sm: "text-sm py-md px-xl",
+      lg: "text-md py-lg px-xl",
+    },
+    shape: {
+      rectangle: "rounded-md",
+      pill: "rounded-full",
+    },
+    isContained: {
+      false: clsx(
+        "border border-primary hover:border-primary-hover disabled:border-primary-disabled",
+        "bg-card hover:bg-gray-5",
+        "text-primary disabled:text-text-disabled"
+      ),
+      true: clsx(
+        "bg-primary hover:bg-primary-hover disabled:bg-primary-disabled",
+        "text-text-button disabled:text-text-disabled"
+      ),
+    },
+    disabled: {
+      false: "cursor-pointer",
+      true: "",
+    },
+  },
+})
+
 type ButtonProps = {
-  children: ReactNode
+  size?: "sm" | "lg"
+  shape?: "rectangle" | "pill"
+  isContained?: boolean
+  disabled?: boolean
+  children?: ReactNode
 }
-const Button = ({ children }: DefaultButtonProps & ButtonProps) => {
-  // NOTE: inputs 폴더의 placeholder 용입니다
-  return <button>{children}</button>
+const Button = ({
+  size = "sm",
+  shape = "rectangle",
+  isContained = true,
+  disabled = false,
+  children,
+  ...props
+}: DefaultButtonProps & ButtonProps) => {
+  const { className, ...rest } = props
+  return (
+    <>
+      <button
+        {...rest}
+        disabled={disabled}
+        className={clsx(
+          buttonVariants({ size, shape, isContained, disabled }),
+          className
+        )}
+      >
+        {children}
+      </button>
+    </>
+  )
 }
 
 export default Button
