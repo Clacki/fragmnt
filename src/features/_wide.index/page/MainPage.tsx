72f6fe617bfcf764a544ee9fc19d8f3da756781f
ThePott <134694901+ThePott@users.noreply.github.com>
2026-04-17 13:09:46 +0900

    feat: main page 레이아웃 구성 (기능 없이) (#102)
    * feat: 폴더 및 파일 생성 (#83)
    
    * feat: find your scent 섹션 레이아웃 제작 (#93)
    
    * feat: QuickStart 섹션 레이아웃 설정 (#93)
    
    * feat: 리뷰 페이지 레이아웃 제작 (#93)
    
    * feat: FAQ 레이아웃 설정 (#93)
    
    * design: 버튼 children css 수정 (#93)
    
    * design: quickstart card container의 너비 최대로 수정 (#93)

--- src/features/_wide.index/page/MainPage.tsx
+++ src/features/_wide.index/page/MainPage.tsx
@@ -1,15 +1,22 @@
-import { Button } from "@/shared/components"
-import { useNavigate } from "@tanstack/react-router"
+import { Vstack } from "@/shared/components"
+import Introduction from "./1-introduction/Introduction"
+import FindYourScent from "./2-find-your-scent/FindYourScent"
+import ViewAllScents from "./3-view-all-scents/ViewAllScents"
+import QuickStart from "./4-quick-start/QuickStart"
+import CustomerReviews from "./5-customer-reviews/CustomerReviews"
+import FAQ from "./6-faq/FAQ"
 
 const MainPage = () => {
-  const navigate = useNavigate()
   return (
-    <div>
-      <p>this is main page placeholder</p>
-      <Button onClick={() => navigate({ to: "/not-real" })}>
-        navigate to "not-real" page
-      </Button>
-    </div>
+    <Vstack gap="2xl" className="pb-2xl">
+      <Introduction />
+      <FindYourScent />
+      <ViewAllScents />
+      <div>perfume image placeholder</div>
+      <QuickStart />
+      <CustomerReviews />
+      <FAQ />
+    </Vstack>
   )
 }
 
