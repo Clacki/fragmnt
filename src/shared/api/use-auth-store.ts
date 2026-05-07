f4d81a50640864c213ad9d12d2ef9ab930a417d7
ThePott <134694901+ThePott@users.noreply.github.com>
2026-05-07 14:04:58 +0900

    fix: 소설 로그인 후 access token, profile 받아오기 (#296)
    * feat: 소셜 로그인 후 액세스 토큰 및 프로필 받아 저장 (#293)
    
    * fix: 라우트 주소 참조명 수정 (#293)
    
    * chore: rebase 충돌 해결 (#293)

--- src/shared/api/use-auth-store.ts
+++ src/shared/api/use-auth-store.ts
@@ -2,6 +2,7 @@ import { logoutApi } from "@/features/my-page/api/user.api"
 import { create } from "zustand"
 import { createJSONStorage, persist } from "zustand/middleware"
 import type { Profile } from "../types"
+import { instance, plainInstance } from "./axios-instance"
 
 type AuthStoreState = {
   accessToken: string | null
@@ -10,6 +11,8 @@ type AuthStoreState = {
   profile: Profile | null
   setProfile: (profile: Profile | null) => void
 
+  refresh: () => Promise<void>
+  getProfile: (accessToken: string) => Promise<void>
   logout: () => Promise<void>
 
   clearAuth: () => void
@@ -17,12 +20,35 @@ type AuthStoreState = {
 
 const useAuthStore = create<AuthStoreState>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       accessToken: null,
       setAccessToken: (accessToken) => set({ accessToken }),
 
       profile: null,
       setProfile: (profile) => set({ profile }),
+
+      refresh: async () => {
+        const refreshResponse = await instance.post<{ access: string }>(
+          "/accounts/me/refresh"
+        )
+        const { access } = refreshResponse.data
+        set({ accessToken: access })
+
+        const getProfile = get().getProfile
+        await getProfile(access)
+      },
+      getProfile: async (accessToken) => {
+        const response = await plainInstance.get<Profile>(
+          "/accounts/me/profile",
+          {
+            headers: { Authorization: `Bearer ${accessToken}` },
+            withCredentials: true,
+          }
+        )
+
+        const profile = response.data
+        set({ profile })
+      },
 
       logout: async () => {
         try {
